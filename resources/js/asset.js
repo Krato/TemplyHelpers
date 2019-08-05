97500b0c5b49d996c6af1b9b219cfc30684577a4
Eric Lagarda <info@ericlagarda.com>
2019-08-05 13:34:06 +0200

    View to edit

--- resources/js/asset.js
+++ resources/js/asset.js
@@ -2,6 +2,29 @@
  * Nova Temply helpers
  */
 
+async function fetchAsyncConfigs () {
+  // await response of fetch call
+  let response = await fetch('/api/v1/nova-config');
+  // only proceed once promise is resolved
+  let data = await response.json();
+  // only proceed once second promise is resolved
+  return data;
+}
+
+var resources;
+
+if (localStorage.getItem("temply.edit-link-resources") === null) {
+    fetchAsyncConfigs()
+    .then(data => {
+        resources = data;
+        localStorage.setItem("temply.edit-link-resources", resources);
+        
+    })
+    .catch(reason => console.log(reason.message))
+} else {
+    resources = localStorage.getItem("temply.edit-link-resources")
+}
+
 import { checkNewPage } from './helpers/removeEditButton';
 
 Nova.booting((Vue, router) => {
@@ -16,7 +39,21 @@ Nova.booting((Vue, router) => {
             next(newTo);
         }
 
+        if (resources.includes(from.params.resourceName)) {
+            if (from.name == 'index' && to.name == 'detail') {
+                let newTo = {
+                    name : 'edit',
+                    params: {
+                        resourceName: to.params.resourceName,
+                        resourceId: to.params.resourceId.toString()
+                    }
+                }
+                next(newTo)
+            }
+        }
+
         checkNewPage(router);
         next();
     });
+
 });
